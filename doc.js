cb3829e5a189aa4bd501a1c031adaf30aab51a19
Brad Hezh <bradhezh@gmail.com>
2025-05-26 05:19:02 +1200

    publish docs with modified doc.js to find both docs and doc

--- doc.js
+++ doc.js
@@ -1,9 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
   const nav = document.querySelector('.tsd-toolbar-contents')
   if (nav) {
-    const paths = location.pathname.split('/')
-    const i = paths.lastIndexOf('docs')
-    const depth = paths.length - i - 2
+    const paths = location.pathname.split('/').filter(Boolean)
+    let last = -1
+    for (let i = paths.length - 1; i >= 0; --i) {
+      if (/doc[s]?$/.test(paths[i])) {
+        last = i
+        break
+      }
+    }
+    const depth = paths.length - last - 2
     const up = Array(depth).fill('..').join('/') || '.'
     const link = document.createElement('a')
     link.href = `${up}/index.html`
